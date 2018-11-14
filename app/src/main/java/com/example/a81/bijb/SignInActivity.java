c4b2696baa45f4dce520257ebcc01fd29566ea68
8.1 <marshachikita16@gmail.com>
2018-11-14 16:32:32 +0700

    Tampilan

--- app/src/main/java/com/example/a81/bijb/SignInActivity.java
+++ app/src/main/java/com/example/a81/bijb/SignInActivity.java
@@ -7,20 +7,28 @@ import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
-//    TextView tv1, tv2;
-//    Typeface calibri_bold;
+    TextView tv1, tv2, t_user, t_pass, t1, t2;
+    Typeface calibri_bold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-//        tv1 = (TextView) findViewById(R.id.bijb);
-//        tv2 = (TextView) findViewById(R.id.signin);
-//
-//        calibri_bold = Typeface.createFromAsset(getAssets(), "calibrib.ttf");
-//
-//        tv1.setTypeface(calibri_bold);
-//        tv2.setTypeface(calibri_bold);
+        tv1 = (TextView) findViewById(R.id.bijb);
+        tv2 = (TextView) findViewById(R.id.signin);
+        t_user = (TextView) findViewById(R.id.t_user);
+        t_pass = (TextView) findViewById(R.id.t_pass);
+        t1 = (TextView) findViewById(R.id.t_forget);
+        t2 = (TextView) findViewById(R.id.t_connect);
+
+        calibri_bold = Typeface.createFromAsset(getAssets(), "calibrib.ttf");
+
+        tv1.setTypeface(calibri_bold);
+        tv2.setTypeface(calibri_bold);
+        t_user.setTypeface(calibri_bold);
+        t_pass.setTypeface(calibri_bold);
+        t1.setTypeface(calibri_bold);
+        t2.setTypeface(calibri_bold);
     }
 }
--- app/src/main/java/com/example/a81/bijb/SignUpActivity.java
+++ app/src/main/java/com/example/a81/bijb/SignUpActivity.java
@@ -1,13 +1,32 @@
 package com.example.a81.bijb;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    TextView user, email, password, gender;
+    Typeface calibri_bold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        user = (TextView) findViewById(R.id.t_user);
+        email = (TextView) findViewById(R.id.t_email);
+        password = (TextView) findViewById(R.id.t_pass);
+        gender = (TextView) findViewById(R.id.t_gender);
+
+        calibri_bold = Typeface.createFromAsset(getAssets(), "calibrib.ttf");
+
+        user.setTypeface(calibri_bold);
+        email.setTypeface(calibri_bold);
+        password.setTypeface(calibri_bold);
+        gender.setTypeface(calibri_bold);
     }
 }
